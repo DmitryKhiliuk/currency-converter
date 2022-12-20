81b58e0812e00483efcd8cb3f9a5d54810669899
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-12-20 22:09:47 +0300

    app links test

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {AppRootStateType} from "./store";
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import {MemoryRouter} from "react-router-dom";
 import * as reduxHooks from "react-redux";
 import App from "./App";
+import {Navigation} from "../Components/Navigation/Navigation";
+import userEvent from "@testing-library/user-event";
+import {Converter} from "../Components/Converter/Converter";
+
+window.matchMedia = window.matchMedia || function() {
+    return {
+        matches: false,
+        addListener: function() {},
+        removeListener: function() {}
+    };
+}; // заглушка
 
 let startState: AppRootStateType
 
@@ -12,6 +23,7 @@ const mockedSelector = jest.spyOn(reduxHooks, 'useSelector')
 const mockedDispatch = jest.spyOn(reduxHooks, 'useDispatch')
 
 describe('app test', () => {
+
     beforeEach(() => {
         startState = {
             rate: {
@@ -34,4 +46,28 @@ describe('app test', () => {
         const view = render(<MemoryRouter><App/></MemoryRouter>)
         expect(view).toMatchSnapshot()
     })
+    it('should render converter component', () => {
+        render(<MemoryRouter><App/></MemoryRouter>)
+        /*const view = render(<MemoryRouter><Converter currencyMain={['USD', 'EUR', 'GBP', 'PLN', 'CHF']}
+                                                     currencyCode={['USD', 'EUR', 'GBP', 'PLN', 'CHF','AFN']}
+                                                     currencyAncillary={['AED','AFN']}
+                                                     quantity={1}
+                                                     setQuantity={jest.fn()}
+        /></MemoryRouter>)*/
+        const input = screen.getAllByRole('spinbutton')
+        const converterLink = screen.getByText('Converter')
+        userEvent.click(converterLink)
+        expect(input.length).toBe(2)
+    })
+    it('should render rate component', () => {
+        render(<MemoryRouter><App/></MemoryRouter>)
+        const rateLink = screen.getByText('Rate')
+        userEvent.click(rateLink)
+        const input = screen.getByRole('spinbutton')
+        const text = screen.getByText(/Last update/i)
+        expect(input).toBeInTheDocument()
+        expect(text).toBeInTheDocument()
+
+    })
+
 })
---
+++ src/Components/Navigation/Navigation.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {render, screen} from "@testing-library/react";
+import {MemoryRouter} from "react-router-dom";
+import App from "../../App/App";
+import {Navigation} from "./Navigation";
+
+describe('navigation component test', () => {
+    it('app render test', () => {
+        const view = render(<MemoryRouter><Navigation/></MemoryRouter>)
+        expect(view).toMatchSnapshot()
+    })
+    it('buttons render test', () => {
+        render(<MemoryRouter><Navigation/></MemoryRouter>)
+        const allButtons = screen.getAllByRole('button')
+        const btnConverter = screen.getByText('Converter')
+        const btnRate = screen.getByText('Rate')
+        expect(allButtons.length).toBe(2)
+        expect(btnConverter).toBeInTheDocument()
+        expect(btnRate).toBeInTheDocument()
+    })
+})
